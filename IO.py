3bb2f4438a671ac5ceb7989b188869628530d0c5
suhani1319 <suhanisheikh1319@gmail.com>
2026-01-31 08:11:52 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,9 @@
+# output using print
+print("hello world!..")
+print("suhani")
+
+# input using input
+Name=input("enter your name:")
+print("hello",Name)
+age= input("enter your age:")
+print("years",age)
