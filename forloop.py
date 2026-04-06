015ff8eb8abac2b2cc41a2d6e2f34c9d751376b1
suhani1319 <suhanisheikh1319@gmail.com>
2026-04-06 22:10:43 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,24 @@
+# print number from 1 to 10
+for i in range (1,11):
+    print(i)
+
+# print even numbers
+for i in range (1,21):
+    if i % 2 ==0:
+        print(i)
+
+# table of 5
+for i in range(1,11):
+   print("5*",i,"=",5*i)
+
+#sum 1 to 5
+total=0
+for i in range(1,6):
+    total=total+i
+    print("sum is:",total) 
+
+#print list elements
+numbers=[10,20,30,40,50]
+for n in numbers:
+    print(n)
+
